0c2696d20c2aa66b43438de80a64ede459dd0175
danielsteman <daniel-steman@live.nl>
2022-05-23 20:53:01 +0200

    refactored scraper component

--- src/scraper.py
+++ src/scraper.py
@@ -10,54 +10,14 @@ from mapper import Mapper
 from configurator import Config
 from transformer import Transformer
 
-if os.getenv("DEBUG") == "True":
+if os.getenv("DEBUG"):
     logging.basicConfig(level=logging.INFO)
-
-def driver_init(remote_address: str = None):
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--headless")
-    if remote_address:
-        return webdriver.Chrome(
-            command_executor=remote_address,
-            options=chrome_options
-        )
-    return webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options
-    )
-
-def extract(driver, *, identifier: str, url: str):
-    driver.get(url)
-    result = driver.find_elements_by_class_name(identifier)
-    if isinstance(result, list):
-        return [e.text for e in result]
-    return result
-
-def transform(data):
-    mapper = Mapper(data)
-    if len(data) == 8:
-        mapper._move_item_to_end(0)
-    mapper.map()
-    return mapper.mapped_data
-
-def etl(estate_agent: str, *, config: Config):
-    identifier = config.get('agents', estate_agent, 'class')
-    url = config.get('agents', estate_agent, 'url')
-    driver = driver_init()
-    data = extract(driver, identifier=identifier, url=url)
-    parsed_data = [listing.split('\n') for listing in data]
-    collection = [transform(line) for line in parsed_data]
-
-    loader = Loader()
-    loader.load(collection)
-
-    driver.close()
 
 
 class Scraper:
     def __init__(
         self,
+        *,
         config: Config,
         estate_agent: str,
         extractor: Extractor,
@@ -76,6 +36,7 @@ class Scraper:
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless")
         self.chrome_options.add_argument('--no-sandbox')
+        self.chrome_options.add_argument('--disable-dev-shm-usage')
     
     def __enter__(self):
         self.driver = webdriver.Chrome(
@@ -84,6 +45,7 @@ class Scraper:
         )
         if self.remote:
             self.driver.command_executor = self.remote
+        return self
 
     def etl(self):
         extractor = self.extractor(self.driver, self.config)
